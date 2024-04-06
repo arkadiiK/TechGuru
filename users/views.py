5fddec6c6ad3d23af6ef5a33c60b2673e4aad270
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-06 20:00:45 +0300

    Added registration and login view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import User, RegisterForm
+from .models import User
+from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login, logout
 
 
+# home page
+def index(request):
+    return render(request, 'index.html')
+
+
+# registration page
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -22,3 +30,25 @@ def register(request):
             form = RegisterForm()
 
         return render(request, 'registration/register.html', {'form': form})
+
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+        else:
+            form = LoginForm()
+            return render(request, 'login.html', {'form': form})
+
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
