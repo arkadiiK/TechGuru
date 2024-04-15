d0a813d09d23dbc8f8f8ec9cb5618ba779f4fc99
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-15 19:02:34 +0300

    Trying to fix a SystemCheckError

--- users/models.py
+++ users/models.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
+
+    user_groups = models.ManyToManyField(Group, related_name='custom_user_groups', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'surname', 'email', 'password']
+    REQUIRED_FIELDS = ['name', 'surname']
 
     def __str__(self):
         return self.email
