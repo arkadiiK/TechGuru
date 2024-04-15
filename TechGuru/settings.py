2b9b7c1f2827a5f94d61981269e269e39dd06f5e
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-15 22:35:54 +0300

    AUTH_USER_MODEL added

--- TechGuru/settings.py
+++ TechGuru/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# AUTH_USER_MODEL = 'users.models.User'
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
