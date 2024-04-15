845582b44517f999df561102042f25a5d7b84a2d
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-15 22:35:36 +0300

    User class rebranding, have the different error now

--- users/models.py
+++ users/models.py
@@ -1,14 +1,11 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
-
-    user_groups = models.ManyToManyField(Group, related_name='custom_user_groups', blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions', blank=True)
+    email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'surname']
