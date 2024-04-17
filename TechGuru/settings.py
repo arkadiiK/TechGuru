7a9e27a4e96596732b0772136d4aaf42721a442d
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-17 22:29:07 +0300

    HOSTS added

--- TechGuru/settings.py
+++ TechGuru/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     # local
     'cart',
     'orders',
