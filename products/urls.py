91a27521dffb64128fb412c62e188a4b68d69fed
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-24 19:47:50 +0300

    new urls

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from products import views
+from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('products/<int:product_id>', views.product_detail, name='product_detail'),
-    path('add_to_cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
-    path('view_cart/', views.view_cart, name='view_cart'),
-    path('checkout/', views.checkout, name='checkout')
+    path('products/', views.product_list, name='product_list'),
+    path('add-product/', views.add_product, name='add_product')
+
 ]
