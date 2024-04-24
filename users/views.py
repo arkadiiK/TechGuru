755882e3e20ca7a7e3c7c83d3e14ac5319109904
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-24 19:50:33 +0300

    redirect edited

--- users/views.py
+++ users/views.py
@@ -35,7 +35,7 @@ def user_login(request):
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('base')
             else:
                 # Handle authentication failure
                 return render(request, 'login.html', {'form': form, 'error': 'Invalid email or password'})
