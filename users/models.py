147c54f741afbdfb44878817f7391f9efab64629
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-24 18:27:50 +0300

    Renamed REQUIRED_FIELDS

--- users/models.py
+++ users/models.py
@@ -31,7 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'surname']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
