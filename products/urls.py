acbc49cb4697496dd8494296b3235c5dca6772b1
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-25 22:57:19 +0300

    new path

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'),
+    path('', views.product_list, name='product_list'),
     path('add-product/', views.add_product, name='add_product')
 
 ]
