bc173f62024b589f342bc81017fbf5e8db3b9fcf
GOVNOLORD <kirilov.1080@gmail.com>
2024-04-29 16:52:40 +0300

    edited path to photos

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='media/')
 
     def __str__(self):
         return self.name
