d8618a8a7693bca4562ed493a71cc088c1f66f14
GOVNOLORD <kirilov.1080@gmail.com>
2024-05-02 21:21:28 +0300

    deleted import that was not in use

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 from users.models import User
@@ -30,4 +30,3 @@ class LoginForm(forms.Form):
                 self.add_error(None, self.error_messages['invalid_login'])
 
         return cleaned_data
-
