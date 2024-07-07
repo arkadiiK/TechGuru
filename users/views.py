28670dec110d4aba76fb002c7bc2b8af60665aca
Kyrylov Arkadii <kirilov.1080@gmail.com>
2024-07-07 18:46:42 +0300

    reworked views, logoutView deleted

--- users/views.py
+++ users/views.py
@@ -1,28 +1,21 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 from .models import User
-from .forms import RegisterForm, LoginForm
-from django.views import View
-from common.mixins import RegistrationMixin, TitleMixin, LoginMixin
+from .forms import RegisterForm
+from common.mixins import TitleMixin
 
 
-class RegisterView(RegistrationMixin, FormView, TitleMixin):
+class RegisterView(FormView, TitleMixin):
     title = 'Registration'
     model = User
     form_class = RegisterForm
-    template_name = 'register.html'
+    template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
     success_message = "Registration successful. You can now log in."
 
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password1'])
+        user.save()
+        return super().form_valid(form)
 
-class LoginView(LoginMixin, FormView, TitleMixin):
-    title = 'Login'
-    form_class = LoginForm
-    template_name = 'login.html'
-    success_url = reverse_lazy('product_list')
-    success_message = "User logged in."
-
-
-class LogoutView(View):
-    success_url = reverse_lazy('login')
-    success_message = "User logged out."
