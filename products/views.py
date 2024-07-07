ae3a4fb37929d8c5652475c6c18f96d0eaf0c439
Kyrylov Arkadii <kirilov.1080@gmail.com>
2024-07-07 18:37:39 +0300

    reworked views, added mixins and some functions

--- products/views.py
+++ products/views.py
@@ -1,24 +1,20 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
 from .forms import ProductForm
 from .models import Product
-from common.mixins import TitleMixin, AddProductMixin, DeleteProductMixin
+from common.mixins import TitleMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ProductListView(TitleMixin, ListView):
-    title = 'Products'
+    title = 'Tech Guru'
     model = Product
     template_name = 'product_list.html'
     context_object_name = 'products'
 
-    def get_context_data(self, **kwargs):
-        context = super(ProductListView, self).get_context_data(**kwargs)
-        context['products'] = Product.objects.all()
-        return context
 
-
-class AddProductView(TitleMixin, AddProductMixin, ListView, LoginRequiredMixin):
+class AddProductView(LoginRequiredMixin, TitleMixin, FormView, ListView):
     title = 'Add Product'
     model = Product
     form_class = ProductForm
@@ -27,9 +23,21 @@ class AddProductView(TitleMixin, AddProductMixin, ListView, LoginRequiredMixin):
     success_message = "Product Added Successfully"
     login_url = reverse_lazy('login')
 
+    def form_valid(self, form):
+        product = form.save(commit=False)
+        product.save()
+        return super().form_valid(form)
 
-class DeleteProductView(DeleteProductMixin, ListView, LoginRequiredMixin):
+
+class DeleteProductView(LoginRequiredMixin, ListView):
     model = Product
-    success_url = reverse_lazy('product_list')
+    success_url = reverse_lazy('base')
     success_message = "Product Deleted Successfully"
     login_url = reverse_lazy('login')
+
+    def post(self, request, *args, **kwargs):
+        product_id = kwargs.get('product_id')
+        product = get_object_or_404(self.model, id=product_id)
+
+        product.delete()
+        return redirect('product_list')
